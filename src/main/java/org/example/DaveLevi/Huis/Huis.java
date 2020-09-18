6d9eaba8d5880747e2615457c6f2cff354e61dab
Dave Levi <Dave.v.Toor@googlemail.com>
2020-09-18 15:34:43 +0200

    init

---
+++ src/main/java/org/example/DaveLevi/Huis/Huis.java
@@ -0,0 +1,4 @@
+package org.example.DaveLevi.Huis;
+
+public class Huis {
+}
---
+++ src/test/java/org/example/DaveLevi/HuisopdrachtTest/HuisOpdracht.java
@@ -0,0 +1,43 @@
+package org.example.DaveLevi.HuisopdrachtTest;
+
+/*
+Context opdracht = corona
+max 6 andere personen in de woning
+bestaat uit,
+HUIS:   adres huis*
+        int aantalkamers
+        Methodes
+        verantwoordelijkheid
+
+PERSOON: naam
+        leeftijd
+        get/setters
+
+ADRES:  Straat
+        huisnummer
+        stad
+        postcode
+        land
+
+BOA OBJECT: Iets wat kijkt wanneer je over capaciteit bent
+
+ */
+
+
+import org.junit.jupiter.api.Test;
+
+public class HuisOpdracht {
+
+    @Test
+    void bouwEenHuisMetEenAantalKamers() {}
+    @Test
+    void hoeveelMensenMogenErIn() {}
+    @Test
+    void testAdres() {}
+    @Test
+    void maakEenPersoon() {}
+    @Test
+    void bindEenAdres() {}
+    @Test
+    void
+}
